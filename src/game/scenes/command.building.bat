5e17fe5d8ad1160b3474ed2fa5ab5011a87355a5
KaranGoel59 <karangoel59@gmail.com>
2024-12-07 08:16:41 +1100

    fix pipeline

---
+++ src/game/scenes/command.building.bat
@@ -0,0 +1,22 @@
+
+Request Format
+{
+    "command": command_name,
+    "parameters": "..."
+}
+
+
+command.building.GetBuildings
+command.chat.NPCChatUpdate
+command.config.GetBuildingsConfig
+command.config.GetEquipmentsConfig
+command.config.GetNPCsConfig
+command.map.GetMapScene
+command.map.GetMapTown
+command.map.UserNavigate
+command.map.NPCNavigate
+command.map.NPCNavigateTime
+command.npc.GetNPCInfo
+command.npc.GetNPCs
+command.player.GetPlayerInfo
+command.timetick.Tick
