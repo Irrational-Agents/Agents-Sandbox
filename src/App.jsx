81c924fd6bc29d04ccdf63d6cd53af2977708218
kgoel59 <karangoel59@gmail.com>
2024-12-08 10:18:14 +1100

    Stage  (#25)

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useRef } from 'react';
+
+import { PhaserGame } from './game/PhaserGame';
+
+function App () {
+
+    const phaserRef = useRef();
+
+    const currentScene = (scene) => {
+        // Logic to run on scene change
+    }
+    return (
+        <div id="app">
+            <PhaserGame ref={phaserRef} currentActiveScene={currentScene}/>
+        </div>
+    )
+}
+
+export default App
